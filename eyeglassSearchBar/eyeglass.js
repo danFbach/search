1e4cccd2f45f543e2e9e0c4d5f4d3f8e7168c463
danFbach <quiktekrepair@gmail.com>
2016-11-27 22:40:28 -0600

    function update for rhino

--- eyeglassSearchBar/eyeglass.js
+++ eyeglassSearchBar/eyeglass.js
@@ -1,12 +1,40 @@
 $(document).ready(function() {
+	$('.boxContainer').slick({
+		infinite: false,
+	  	slidesToShow: 5,
+	  	slidesToScroll: 4,
+	  	dots: false,
+	  	responsive: [ 
+	  		{
+				breakpoint: 1000,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 2						
+				}	  	
+		  	},
+		  	{
+		  		breakpoint: 640,
+		  		settings: {
+			  		slidesToShow: 2,
+			  		slidesToScroll: 1		  			
+		  		}
+		  	}
+	  	]
+	});
+	$('.boxContainer .slick-disabled').hide();
 	$('#trigger').click(function(){
 		$('.eyeglassContainer, #trigger').toggleClass('js-searchActive');
 		$('form.preload').removeClass('preload');
 		$('.eyeglassContainer input').focus();
 	});
-	$('#boxContainer').slick({
-		infinite: true,
-	  slidesToShow: 5,
-	  slidesToScroll: 4
+	$('input#text').val('');
+	$('button').click(function(){
+		$('.slick-arrow').each(function(){
+			if($(this).hasClass('slick-disabled')){
+				$(this).fadeOut(350);
+			}else{
+				$(this).fadeIn(350);
+			}
+		});
 	});
 });
